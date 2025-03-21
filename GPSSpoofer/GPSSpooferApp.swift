4d326546c2d2862617e2405fdb0acc59248130cb
Alex Iurco <alexandru.iurco@gmail.com>
2025-03-20 20:23:10 -0700

    Initial Commit

---
+++ GPSSpoofer/GPSSpooferApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPSSpooferApp.swift
+//  GPSSpoofer
+//
+//  Created by Alex on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct GPSSpooferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
